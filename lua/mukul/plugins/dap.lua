70e6fea15874f18127c15d7ed816ffd492d520bd
Mukul Sharma <mukulsharma.jan11@gmail.com>
2025-04-26 21:20:20 -0400

    feat: add keyboard shortcuts for dap

--- lua/mukul/plugins/dap.lua
+++ lua/mukul/plugins/dap.lua
@@ -39,7 +39,6 @@ return {
 
 			require("dap-python").setup(python_path)
 
-			-- Optional: Python debugging configuration
 			require("dap").configurations.python = {
 				{
 					type = "python", -- Type of configuration
@@ -51,13 +50,66 @@ return {
 					end,
 				},
 			}
+			-- TODO: test this with neotest
+			-- DAP Keybindings
+			vim.keymap.set(
+				"n",
+				"<leader>dc",
+				require("dap").continue,
+				{ noremap = true, silent = true, desc = "Continue" }
+			)
+			vim.keymap.set("n", "<leader>dp", require("dap").pause, { noremap = true, silent = true, desc = "Pause" })
+			vim.keymap.set(
+				"n",
+				"<leader>do",
+				require("dap").step_over,
+				{ noremap = true, silent = true, desc = "Step Over" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>di",
+				require("dap").step_into,
+				{ noremap = true, silent = true, desc = "Step Into" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>du",
+				require("dap").step_out,
+				{ noremap = true, silent = true, desc = "Step Out" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>db",
+				require("dap").toggle_breakpoint,
+				{ noremap = true, silent = true, desc = "Toggle Breakpoint" }
+			)
+			vim.keymap.set("n", "<leader>dB", function()
+				require("dap").set_breakpoint(vim.fn.input("Condition: "))
+			end, { noremap = true, silent = true, desc = "Set Conditional Breakpoint" })
+			vim.keymap.set(
+				"n",
+				"<leader>du",
+				require("dapui").toggle,
+				{ noremap = true, silent = true, desc = "Toggle DAP UI" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>dv",
+				require("dapui").eval,
+				{ noremap = true, silent = true, desc = "Evaluate Expression" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>dt",
+				require("dap").terminate,
+				{ noremap = true, silent = true, desc = "Terminate Debugger" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>dc",
+				require("dapui").close,
+				{ noremap = true, silent = true, desc = "Close DAP UI" }
+			)
 		end,
-	},
-
-	-- Optional: auto-install debugpy
-	{
-		"jay-babu/mason-nvim-dap.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		config = true,
 	},
 }
