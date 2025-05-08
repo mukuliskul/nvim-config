7d22a1ec71f71abc58347a04ed894a57a147841c
Mukul Sharma <169689349+mukul-mpac@users.noreply.github.com>
2025-05-08 08:55:49 -0400

    fix: breaking changes in mason

--- lua/mukul/plugins/lsp/mason.lua
+++ lua/mukul/plugins/lsp/mason.lua
@@ -7,22 +7,14 @@ return {
 		"neovim/nvim-lspconfig",
 	},
 	config = function()
-		-- import mason
 		local mason = require("mason")
-
-		-- import mason-lspconfig
 		local mason_lspconfig = require("mason-lspconfig")
-
-		-- import mason-tool-installer
 		local mason_tool_installer = require("mason-tool-installer")
-
-		-- import cmp-nvim-lsp plugin
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-		-- import lspconfig
 		local lspconfig = require("lspconfig")
 
-		-- enable mason and configure icons
+		local capabilities = cmp_nvim_lsp.default_capabilities()
+
 		mason.setup({
 			ui = {
 				icons = {
@@ -34,7 +26,6 @@ return {
 		})
 
 		mason_lspconfig.setup({
-			-- list of servers for mason to install
 			ensure_installed = {
 				"ts_ls",
 				"lua_ls",
@@ -46,7 +37,7 @@ return {
 				"yamlls",
 				"marksman",
 			},
-			automatic_installation = true,
+			automatic_enable = true,
 		})
 
 		mason_tool_installer.setup({
@@ -57,77 +48,47 @@ return {
 			},
 		})
 
-		local capabilities = cmp_nvim_lsp.default_capabilities()
+		-- Optional manual configuration overrides
+		lspconfig["lua_ls"].setup({
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+					completion = {
+						callSnippet = "Replace",
+					},
+				},
+			},
+		})
 
-		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({})
-			end,
-			["svelte"] = function()
-				-- configure svelte server
-				lspconfig["svelte"].setup({
-					capabilities = capabilities,
-					on_attach = function(client, bufnr)
-						vim.api.nvim_create_autocmd("BufWritePost", {
-							pattern = { "*.js", "*.ts" },
-							callback = function(ctx)
-								-- Here use ctx.match instead of ctx.file
-								client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-							end,
-						})
-					end,
-				})
-			end,
-			["emmet_ls"] = function()
-				-- configure emmet language server
-				lspconfig["emmet_ls"].setup({
-					capabilities = capabilities,
-					filetypes = {
-						"html",
-						"typescriptreact",
-						"javascriptreact",
-						"css",
-						"sass",
-						"scss",
-						"less",
-						"svelte",
+		lspconfig["pyright"].setup({
+			capabilities = capabilities,
+			settings = {
+				python = {
+					analysis = {
+						typeCheckingMode = "off",
+						autoSearchPaths = true,
+						useLibraryCodeForTypes = true,
+						diagnosticMode = "workspace",
 					},
-				})
-			end,
-			["pyright"] = function()
-				-- configure pyright server
-				lspconfig["pyright"].setup({
-					capabilities = capabilities,
-					settings = {
-						python = {
-							analysis = {
-								typeCheckingMode = "off", -- Disable strict type checking (similar to Pylance)
-								autoSearchPaths = true,
-								useLibraryCodeForTypes = true,
-								diagnosticMode = "workspace", -- Diagnostics for the whole workspace
-							},
-						},
-					},
-				})
-			end,
-			["lua_ls"] = function()
-				-- configure lua server (with special settings)
-				lspconfig["lua_ls"].setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							-- make the language server recognize "vim" global
-							diagnostics = {
-								globals = { "vim" },
-							},
-							completion = {
-								callSnippet = "Replace",
-							},
-						},
-					},
-				})
-			end,
+				},
+			},
+		})
+
+		lspconfig["emmet_ls"].setup({
+			capabilities = capabilities,
+			filetypes = {
+				"html",
+				"typescriptreact",
+				"javascriptreact",
+				"css",
+				"sass",
+				"scss",
+				"less",
+				"svelte",
+			},
 		})
 	end,
 }
