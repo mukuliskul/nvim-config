6bb45d4af2d34e8f586c8071263a3fc788abcf2a
Mukul Sharma <169689349+mukul-mpac@users.noreply.github.com>
2025-09-29 12:07:00 -0400

    feat: use avante ai

---
+++ lua/mukul/plugins/avante.lua
@@ -0,0 +1,58 @@
+return {
+	"yetone/avante.nvim",
+	build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+		or "make",
+	event = "VeryLazy",
+	version = false, -- Never set this value to "*"! Never!
+	opts = {
+		instructions_file = "avante.md",
+		provider = "copilot",
+		providers = {
+			copilot = {}, -- ✅ nothing else needed, copilot.lua manages auth & models
+		},
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		-- optional extras
+		"nvim-mini/mini.pick",
+		"nvim-telescope/telescope.nvim",
+		"hrsh7th/nvim-cmp",
+		"ibhagwan/fzf-lua",
+		"stevearc/dressing.nvim",
+		"folke/snacks.nvim",
+		"nvim-tree/nvim-web-devicons",
+
+		-- GitHub Copilot
+		{
+			"zbirenbaum/copilot.lua",
+			cmd = "Copilot",
+			event = "InsertEnter",
+			config = function()
+				require("copilot").setup({
+					suggestion = { enabled = true, auto_trigger = true },
+					panel = { enabled = true },
+				})
+			end,
+		},
+
+		-- optional extras for images & markdown rendering
+		{
+			"HakonHarnes/img-clip.nvim",
+			event = "VeryLazy",
+			opts = {
+				default = {
+					embed_image_as_base64 = false,
+					prompt_for_file_name = false,
+					drag_and_drop = { insert_mode = true },
+					use_absolute_path = true,
+				},
+			},
+		},
+		{
+			"MeanderingProgrammer/render-markdown.nvim",
+			opts = { file_types = { "markdown", "Avante" } },
+			ft = { "markdown", "Avante" },
+		},
+	},
+}
