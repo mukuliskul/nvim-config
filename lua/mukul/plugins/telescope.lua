cba16a512af6328faaceb8d0b4dae4415d22a54d
Mukul Sharma <mukul@mpac.io>
2026-01-19 09:36:30 -0500

    feat: use option for navigation in telescope

--- lua/mukul/plugins/telescope.lua
+++ lua/mukul/plugins/telescope.lua
@@ -17,8 +17,8 @@ return {
 				-- path_display = { "smart" },
 				mappings = {
 					i = {
-						["<C-k>"] = actions.move_selection_previous, -- move to prev result
-						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<M-k>"] = actions.move_selection_previous, -- move to prev result
+						["<M-j>"] = actions.move_selection_next, -- move to next result
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 					},
 				},
